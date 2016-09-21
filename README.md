4087093ed10fe94f2aaabf4a851d2b8f521f5b89
michbr <bmichaud45@gmail.com>
2016-09-21 15:07:12 -0600

    Add a script to easily regenerate the schema. Hopefully I'll include this in the build somehow at a later date.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ActionCast
+Regenerate the schema file with the following command:
+schemagen DataModel.java Director.java SongCast.java Person.java Role.java RoleType.java Session.java Song.java UniqueItem.java RoleAssignment.java
---
+++ generate_schema.sh
@@ -0,0 +1,3 @@
+#! /bin/bash 
+
+schemagen src/main/java/action_cast/model/DataModel.java src/main/java/action_cast/model/Director.java src/main/java/action_cast/model/SongCast.java src/main/java/action_cast/model/Person.java src/main/java/action_cast/model/Role.java src/main/java/action_cast/model/RoleType.java src/main/java/action_cast/model/Session.java src/main/java/action_cast/model/Song.java src/main/java/action_cast/model/UniqueItem.java src/main/java/action_cast/model/RoleAssignment.java
