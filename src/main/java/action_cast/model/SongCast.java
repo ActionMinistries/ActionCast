2c32af78aa304e1f923d69958a4ec4881ca67d2d
michbr <bmichaud45@gmail.com>
2016-09-30 11:07:54 -0600

    broken

--- src/main/java/action_cast/model/SongCast.java
+++ src/main/java/action_cast/model/SongCast.java
@@ -1,9 +1,7 @@
 package action_cast.model;
 
 import javax.xml.bind.annotation.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by bmichaud on 8/31/2015.
@@ -16,7 +14,7 @@ public class SongCast extends UniqueItem {
     private Song song;
 
     @XmlTransient
-    private Map<Role, RoleAssignment> assignmentMap;
+    private Map<Role, List<RoleAssignment>> assignmentMap;
 
     @XmlElementWrapper
     private final HashSet<RoleAssignment> assignments = new HashSet<>();
@@ -34,11 +32,17 @@ public class SongCast extends UniqueItem {
         return song;
     }
 
-    public Map<Role, RoleAssignment> getAssignmentMap() {
+    public Map<Role, List<RoleAssignment>> getAssignmentMap() {
         if (assignmentMap == null) {
             assignmentMap = new HashMap<>();
             for (RoleAssignment assignment : assignments) {
-                assignmentMap.put(assignment.getRole(), assignment);
+                if (assignmentMap.containsKey(assignment.getRole())) {
+                    assignmentMap.get(assignment.getRole()).add(assignment);
+                } else {
+                    List<RoleAssignment> assignmentList = new ArrayList<>();
+                    assignmentList.add(assignment);
+                    assignmentMap.put(assignment.getRole(), assignmentList);
+                }
             }
         }
         return assignmentMap;
@@ -48,12 +52,15 @@ public class SongCast extends UniqueItem {
         return assignments;
     }
 
-    public RoleAssignment assign(Person performer, Role role) {
+    public RoleAssignment assign(Person performer, Role role, int roleAssignmentId) {
         RoleAssignment roleAssignment;
         if (!getAssignmentMap().containsKey(role)) {
+            List<RoleAssignment> assignmentList = new ArrayList<>();
             roleAssignment = new RoleAssignment(assignments.size(), performer, role, this);
+            assignmentList.add(roleAssignment);
+
             assignments.add(roleAssignment);
-            getAssignmentMap().put(role, roleAssignment);
+            getAssignmentMap().put(role, assignmentList);
 
         } else {
             if (performer == null) {
@@ -61,6 +68,7 @@ public class SongCast extends UniqueItem {
                 getAssignmentMap().remove(role);
                 return null;
             } else {
+
                 getAssignmentMap().get(role).setPerson(performer);
             }
         }
